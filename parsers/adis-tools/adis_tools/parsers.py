a64620165e05ad4b92da84d14197b44086affb46
Marnik Bercx <mbercx@gmail.com>
2024-01-18 15:50:44 +0100

    👌 Update the basic EOS example

--- parsers/adis-tools/adis_tools/parsers.py
+++ parsers/adis-tools/adis_tools/parsers.py
@@ -14,25 +14,38 @@ def parse_pw(xml_file):
 
     xml_dict = XMLSchema(files(schemas) / 'qes_230310.xsd').to_dict(xml_file)
 
-    cell = numpy.array(
-        [ v for v in xml_dict['output']['atomic_structure']['cell'].values()]
-    ) * CONSTANTS.bohr_to_ang
-    symbols = [el['@name'] for el in xml_dict['output']['atomic_structure']['atomic_positions']['atom']]
-    positions = numpy.array(
-        [el['$'] for el in xml_dict['output']['atomic_structure']['atomic_positions']['atom']]
-    ) * CONSTANTS.bohr_to_ang
+    parsed_results = {}
 
-    atoms = Atoms(
-        cell=cell,
-        positions=positions,
-        symbols=symbols,
-        pbc=True,
-    )
-    return {
-        'ase_structure': atoms,
-        'energy': xml_dict['output']['total_energy']['etot'] * 2 * CONSTANTS.ry_to_ev,
-        'forces': (
+    try:
+        cell = numpy.array(
+            [ v for v in xml_dict['output']['atomic_structure']['cell'].values()]
+        ) * CONSTANTS.bohr_to_ang
+        symbols = [el['@name'] for el in xml_dict['output']['atomic_structure']['atomic_positions']['atom']]
+        positions = numpy.array(
+            [el['$'] for el in xml_dict['output']['atomic_structure']['atomic_positions']['atom']]
+        ) * CONSTANTS.bohr_to_ang
+
+        parsed_results['ase_structure'] = Atoms(
+            cell=cell,
+            positions=positions,
+            symbols=symbols,
+            pbc=True,
+        )
+    except KeyError:
+        pass
+
+    try:
+        parsed_results['energy'] = xml_dict['output']['total_energy']['etot'] * CONSTANTS.ry_to_ev
+    except KeyError:
+        pass
+
+    try:
+        parsed_results['forces'] = (
             numpy.array(xml_dict['output']['forces']['$']).reshape(xml_dict['output']['forces']['@dims'])
             * 2 * CONSTANTS.ry_to_ev / CONSTANTS.bohr_to_ang
         )
-    }
+    except KeyError:
+        pass
+
+    return parsed_results
+
